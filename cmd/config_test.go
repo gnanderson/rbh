99061ba303a69de7af9f4d0ceda9320c593303cc
Graham Anderson <graham.anderson@gmail.com>
2019-07-08 16:12:00 +0100

    chore: test example config in case of breakages

---
+++ cmd/config_test.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+type gString func(string) string
+type gInt func(string) int
+type gBool func(string) bool
+
+var getString gString = func(key string) string { return viper.GetString(key) }
+var getInt gInt = func(key string) int { return viper.GetInt(key) }
+var getBool gBool = func(key string) bool { return viper.GetBool(key) }
+
+var cfgTests = []struct {
+	key string
+	val interface{}
+	f   interface{}
+}{
+	{"user", "testnetadmin", getString},
+	{"passwd", "testnetpasswd", getString},
+	{"addr", "192.168.254.10", getString},
+	{"port", 6006, getInt},
+	{"tls", true, getBool},
+	{"banlength", 1440, getInt},
+	{"repeat", 60, getInt},
+}
+
+// well this is a little verbose but I don't want to break the example config
+func TestExampleConfig(t *testing.T) {
+	cfgFile = "../examples/.rbh.yaml"
+	initConfig()
+
+	tf := func(key string, result interface{}, val interface{}) {
+		resultKind := reflect.TypeOf(result).Kind()
+		valKind := reflect.TypeOf(val).Kind()
+		if resultKind != valKind || result != val {
+			t.Errorf("unexpected type from config '%s' - %T, expected %T", key, resultKind, valKind)
+		}
+	}
+
+	for _, tt := range cfgTests {
+		t.Run(tt.key, func(t *testing.T) {
+			var strResult string
+			var intResult int
+			var boolResult bool
+
+			switch f := tt.f.(type) {
+			case gString:
+				strResult = f(tt.key)
+				tf(tt.key, strResult, tt.val)
+			case gInt:
+				intResult = f(tt.key)
+				tf(tt.key, intResult, tt.val)
+			case gBool:
+				boolResult = f(tt.key)
+				tf(tt.key, boolResult, tt.val)
+			}
+		})
+	}
+}
